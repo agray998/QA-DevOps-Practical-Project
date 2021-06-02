439d029b28e8756d2875e03be3beac537e4ceb2a
agray998 <a.gray.4797@gmail.com>
2021-06-02 08:37:46 +0000

    Added sql image files

--- front-end/application/__init__.py
+++ front-end/application/__init__.py
@@ -4,7 +4,8 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('db_uri')
+password = os.getenv('password')
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{password}@eventsdb:3306/events-db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
--- front-end/application/models.py
+++ front-end/application/models.py
@@ -2,9 +2,9 @@ from application import db
 
 class Events(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    units = db.Column(db.String(50))
-    effect = db.Column(db.String(50))
+    event_name = db.Column(db.String(50))
+    unit_type = db.Column(db.String(50))
+    status_effect = db.Column(db.String(50))
     date_generated = db.Column(db.DateTime)
     def __repr__(self):
         return f"{self.name}: {self.units} units experience {self.effect}"
