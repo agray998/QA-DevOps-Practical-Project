6068ea17b56a1e8989a183916e67ad1180ede291
agray998 <a.gray.4797@gmail.com>
2021-06-02 13:52:58 +0000

    fixed syntax errors in sql file

--- events-db/Create.sql
+++ events-db/Create.sql
@@ -1,9 +1,10 @@
-CREATE DATABASE IF NOT EXISTS events-db;
-USE events-db;
+CREATE DATABASE IF NOT EXISTS eventsdb;
+USE eventsdb;
 CREATE TABLE IF NOT EXISTS events (
-    id = INTEGER NOT NULL AUTO_INCREMENT,
-    event_name = VARCHAR(50) NOT NULL
-    unit_type = VARCHAR(50) NOT NULL
-    status_effect = VARCHAR(50) NOT NULL
-    date_generated = DATE NOT NULL
+    id INT AUTO_INCREMENT,
+    event_name VARCHAR(50) NOT NULL,
+    unit_type VARCHAR(50) NOT NULL,
+    status_effect VARCHAR(50) NOT NULL,
+    date_generated DATE NOT NULL,
+    PRIMARY KEY (id)
 );
--- front-end/application/__init__.py
+++ front-end/application/__init__.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 password = os.getenv('password')
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{password}@eventsdb:3306/events-db"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{password}@eventsdb:3306/eventsdb"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
