c84713a5d1627a7bee9700fd755f953961790682
agray998 <a.gray.4797@gmail.com>
2021-06-02 10:50:27 +0000

    replaced underscores with dashes

--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -8,9 +8,9 @@ from datetime import date
 # home route
 @app.route('/', methods = ['GET'])
 def home():
-    event_name = requests.get('http://name_api:5000/get_name')
-    unit_type = requests.get('http://unit_api:5000/get_unit')
-    effect = requests.post('http://effect_api:5000/get_effect', json = {"Event": event_name.text, "Unit": unit_type.text})
+    event_name = requests.get('http://name-api:5000/get_name')
+    unit_type = requests.get('http://unit-api:5000/get_unit')
+    effect = requests.post('http://effect-api:5000/get_effect', json = {"Event": event_name.text, "Unit": unit_type.text})
     event = Events(name = event_name.text, units = unit_type.text, effect = effect.text, date_generated = date.today())
     db.session.add(event)
     db.session.commit()
